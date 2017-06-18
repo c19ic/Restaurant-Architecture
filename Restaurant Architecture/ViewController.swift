7d13bbb96e893cffdd1ce4725f2bf3e9a85d6516
Isha Chirimar <ishachirimar@Ishas-MacBook.local>
2017-06-18 13:54:49 -0400

    Initial Commit

---
+++ Restaurant Architecture/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Restaurant Architecture
+//
+//  Created by Isha Chirimar on 6/18/17.
+//  Copyright © 2017 Isha Chirimar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
